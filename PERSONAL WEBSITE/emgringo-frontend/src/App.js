a201d1a4dee106fc22aada91e5414c1b9b24e662
Emily Gringorten <emily.gringorten@gmail.com>
2024-09-16 20:35:36 -0700

    initial setup

---
+++ PERSONAL WEBSITE/emgringo-frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hey
+        <div>
+        <p>
+        Welcome to emgringo.com. Still working on this...
+        </p>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
